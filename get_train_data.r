c6a8b2ec0911aedaee2026189d71fce51d5dbdfe
Nikos Mertens <nikos@nikos-mertens.de>
2022-02-20 19:24:11 +0100

    update

--- get_train_data.r
+++ get_train_data.r
@@ -12,15 +12,42 @@ suppressPackageStartupMessages(library(devtools))
 source_url("https://raw.githubusercontent.com/NiksMer/get_manifesto_data/main/functions.R")
 
 # Anwendung
-df <- get_training_data(
+df_roh <- get_training_data(
   api_key = "manifesto_apikey.txt",
   start_day = lubridate::date("1945-01-01"),
   country_vec = c(
-    "United Kingdom"
+    "United Kingdom",
+    "Australia",
+    "New Zealand",
+    "United States",
+    "Canada",
+    "Ireland",
+    "South Africa"
   ),
   reliability = 0.0,
   seed=1
 )
+
+# Daten rausfiltern.
+## USA
+## Die Republikaner verwendeten 2020 das gleiche Wahlprogramm wie 2016. Daher wird 61620_2020 gelöscht.
+df_roh <- df_roh %>%
+    dplyr::filter(corpus_code!="61620_202011")
+
+## Kanada
+## Der Quebec Bloc (62901) schreibt nur französich.
+df_roh <- df_roh %>%
+    dplyr::filter(str_starts(corpus_code,"62901")==FALSE)
+## Im Jahr 2011 sind alle Wahlprogramme auf Französisch.
+df_roh <- df_roh %>%
+    dplyr::filter(corpus_code!="62110_201105") %>%
+    dplyr::filter(corpus_code!="62320_201105") %>%
+    dplyr::filter(corpus_code!="62420_201105") %>%
+    dplyr::filter(corpus_code!="62623_201105") %>%
+    dplyr::filter(corpus_code!="62901_201105")
+
+df <- df_roh %>%
+    select(text,corpus_code,left_right)
 
 # Output
 df_temp <- df %>%
