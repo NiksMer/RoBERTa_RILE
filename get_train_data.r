f516074b01e9ae5368f3a8177797a881a7196eb2
Nikos Mertens <nikos@nikos-mertens.de>
2022-02-23 11:46:23 +0100

    val data

--- get_train_data.r
+++ get_train_data.r
@@ -8,14 +8,26 @@ suppressPackageStartupMessages(library(readr))
 df_roh <- read_csv("https://raw.githubusercontent.com/NiksMer/get_manifesto_data/main/complete_data.csv")
 
 # Filtern auf Trainingsdaten. Canada wird herausgefiltert.
-df_train <- df_roh %>%
+df_train_roh <- df_roh %>%
     dplyr::filter(countryname!="Canada") %>%
     select(text,left_right)
+
+## 80% Trainingsgröße
+smp_size <- floor(0.8 * nrow(df_train_roh))
+
+## Seed definieren für Reproduzierbarkeit
+set.seed(123)
+train_ind <- sample(seq_len(nrow(df_train_roh)), size = smp_size)
+
+# Daten schneiden
+df_train <- df_train_roh[train_ind, ]
+df_val <- df_train_roh[-train_ind, ]
 
 df_test <- df_roh %>%
     dplyr::filter(countryname=="Canada") %>%
     select(text,left_right,corpus_code)
 
 # Daten speichern
-write_csv(df_train,"trainingsdaten_rile_21022022.csv")
-write_csv(df_test,"testdaten_rile_21022022.csv")
+write_csv(df_train,"trainingsdaten_rile_23022022.csv")
+write_csv(df_val,"validierungsdaten_rile_23022022.csv")
+write_csv(df_test,"testdaten_rile_23022022.csv")
--- tables.r
+++ tables.r
@@ -4,13 +4,19 @@ suppressPackageStartupMessages(library(tidyselect))
 suppressPackageStartupMessages(library(tidyr))
 suppressPackageStartupMessages(library(readr))
 
-df_train <- read_csv("trainingsdaten_rile_21022022.csv") %>%
+df_train <- read_csv("trainingsdaten_rile_23022022.csv") %>%
     group_by(left_right) %>%
     summarise(n=n())
 
 print(df_train)
 
-df_test <- read_csv("testdaten_rile_21022022.csv") %>%
+df_val <- read_csv("validierungsdaten_rile_23022022.csv") %>%
+    group_by(left_right) %>%
+    summarise(n=n())
+
+print(df_val)
+
+df_test <- read_csv("testdaten_rile_23022022.csv") %>%
     group_by(left_right) %>%
     summarise(n=n())
 
